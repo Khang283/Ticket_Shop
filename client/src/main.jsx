97ca44c75d2a365b762d284f84b773123fbf51ea
anvuvip2002 <nguyenphuocanvu@gmail.com>
2024-06-09 20:25:54 +0700

    loading

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import User from './views/User/User.jsx'
+import Booking_QR from './views/booking_QR/Booking.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <User/>
+    <Booking_QR/>
   </React.StrictMode>,
 )
---
+++ client/src/views/receipt/Receipt.jsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from "react";
+
+import DL_Img from "../../assets/DL_Img.png";
+const Receipt = () => {
+  const [ticket, setTicket] = useState("VE_VIP");
+  const [quantity, setQuantity] = useState(0);
+  const [birth, setBirth] = useState(new Date());
+
+  const tickets = [
+    { value: "VE_NGUOI_LON", label: "Vé người lớn" },
+    { value: "VE_TRE_EM", label: "Vé trẻ em" },
+  ];
+  return (
+    <div className="w-full max-w-lg mx-auto">
+      <div className="image">
+        <img
+          src={DL_Img}
+          alt="fireSpot"
+          style={{ width: 1000, marginBottom: 15 }}
+        />
+      </div>
+      <form
+        class="w-full max-w-lg mx-auto"
+        style={{ backgroundColor: "rgb(228 197 158 / 100%)" }}
+      >
+        <div class="flex flex-wrap -mx-3 mb-6 mx-auto">
+          <div class="w-full px-3 mx-auto">
+            <div class="w-full md:w-1/2 px-3 mx-auto">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mx-auto"
+                for="grid-first-name"
+              >
+                Xin chào Nguyễn Phước An Vũ
+              </label>
+            </div>
+
+            <div class="w-full px-3 mx-auto">
+              <p class="text-gray-600 text-xs italic">
+                Vui lòng điền thông tin dưới để thanh toán online
+              </p>
+            </div>
+          </div>
+          <div
+            class="flex flex-wrap -mx-3 mb-6 mx-auto"
+            style={{ marginTop: 10 }}
+          >
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Loại vé
+              </label>
+              <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                placeholder="Doe"
+              />
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Đơn giá
+              </label>
+              <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                placeholder="Doe"
+              />
+            </div>
+          </div>
+
+          <div
+            class="flex flex-wrap -mx-3 mb-6 mx-auto"
+            style={{ marginTop: 5 }}
+          >
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Ngày đi
+              </label>
+              <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="date"
+                placeholder="Doe"
+              />
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Số lượng
+              </label>
+              <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="number"
+                placeholder="Doe"
+              />
+            </div>
+          </div>
+
+          <div
+            class="flex flex-wrap -mx-3 mb-6 mx-auto"
+            style={{ marginTop: 5 }}
+          >
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-last-name"
+            >
+              Tổng
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="number"
+              placeholder="100.000 VNĐ"
+            />
+          </div>
+        </div>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-auto"
+          style={{ marginBottom: 10, backgroundColor: "rgb(128 61 59 / 100%)" }}
+        >
+          Thanh toán
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Receipt;
