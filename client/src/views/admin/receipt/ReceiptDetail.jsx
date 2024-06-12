ef2eca706fd630f1b19eacc6887f6786c943b0d5
TrongNhan22 <20521698@gm.uit.edu.vn>
2024-06-12 11:48:48 +0700

    complete manage  receipt

--- client/src/views/admin/receipt/ReceiptDetail.jsx
+++ client/src/views/admin/receipt/ReceiptDetail.jsx
@@ -1,161 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { format } from "date-fns";
+import vi from "date-fns/locale/vi";
 
-import QR from "../../assets/QR.png";
-const ReceiptDetail = () => {
-  const [ticket, setTicket] = useState("VE_VIP");
-  const [quantity, setQuantity] = useState(0);
-  const [birth, setBirth] = useState(new Date());
+const ReceiptDetail = ({ selectedReceipt, onClose }) => {
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return format(date, 'dd/MM/yyyy', { locale: vi });
+    };
 
-  const tickets = [
-    { value: "VE_NGUOI_LON", label: "Vé người lớn" },
-    { value: "VE_TRE_EM", label: "Vé trẻ em" },
-  ];
-  return (
-    <div className="w-full max-w-lg mx-auto">
+    const formatCurrency = (amount) => {
+        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
+    };
 
-      <form
-        class="w-full max-w-lg mx-auto"
-        style={{ backgroundColor: "rgb(228 197 158 / 100%)" }}
-      >
-        
-        <div class="flex flex-wrap -mx-3 mb-6 mx-auto">
-          <div class="w-full px-3 mx-auto" style={{ marginTop: 5 }}>
-            <div class="w-full md:w-1/2 px-3 mx-auto">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mx-auto"
-                for="grid-first-name"
-              >
-                Đây là đơn đặt vé của ABC
-              </label>
-            </div>
-
-            <div class="w-full px-3 mx-auto">
-              <p class="text-gray-600 text-xs italic">
-                Xin trân trọng cảm ơn
-              </p>
-            </div>
-          </div>
-          <div
-            class="flex flex-wrap -mx-3 mb-6 mx-auto"
-            style={{ marginTop: 5 }}
-          >
-            <div class="w-full md:w-1/2 px-3">
-            <img
-          src={QR}
-          alt="fireSpot"
-          style={{marginBottom: 15 }}
-        />
-            </div>
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
-              >
-                Mã đơn hàng
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="text"
-                placeholder="64Dxiopyhsudo"
-              />
-            </div>
-          </div>
-          <div
-            class="flex flex-wrap -mx-3 mb-6 mx-auto"
-            style={{ marginTop: 10 }}
-          >
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
-              >
-                Loại vé
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="text"
-                placeholder="Doe"
-              />
-            </div>
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
-              >
-                Đơn giá
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="text"
-                placeholder="Doe"
-              />
-            </div>
-          </div>
-
-          <div
-            class="flex flex-wrap -mx-3 mb-6 mx-auto"
-            style={{ marginTop: 5 }}
-          >
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
-              >
-                Ngày đi
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="date"
-                placeholder="Doe"
-              />
-            </div>
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
-              >
-                Số lượng
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="number"
-                placeholder="Doe"
-              />
-            </div>
-          </div>
-
-          <div
-            class="flex flex-wrap -mx-3 mb-6 mx-auto"
-            style={{ marginTop: 5 }}
-          >
-            <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-last-name"
-            >
-              Tổng
-            </label>
-            <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="number"
-              placeholder="100.000 VNĐ"
-            />
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-60 mt-4">
+          <div className="bg-white rounded-lg shadow-lg w-full max-w-md text-left">
+              <h2 className="text-lg mt-8 font-semibold text-center uppercase">Chi tiết hóa đơn</h2>
+              <form className="px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4 flex items-center">
+                  <label className="w-1/3 text-gray-700 text-sm font-bold mb-2 ml-4 mr-4" htmlFor="date">Ngày</label>
+                  <input className="w-2/3 border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text" id="date" value={formatDate(selectedReceipt.date)} readOnly/>
+                </div>
+                <div className="mb-4 flex items-center">
+                  <label className="w-1/3 text-gray-700 text-sm font-bold mb-2 ml-4 mr-4" htmlFor="customer">Khách hàng</label>
+                  <input className="w-2/3 border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text" value={selectedReceipt.user?.full_name || 'Không có tên'} readOnly/>
+                </div>
+                <div className="mb-4 flex items-center">
+                  <label className="w-1/3 text-gray-700 text-sm font-bold mb-2 ml-4 mr-4" htmlFor="customer">Tổng tiền</label>
+                  <input className="w-2/3 border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text" value={formatCurrency(selectedReceipt.total)} readOnly/>
+                </div>
+                <h3 className="mt-2 mb-4 text-md font-semibold text-center uppercase">Chi tiết các vé</h3>
+                <table className="w-full bg-white border-collapse border border-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr className="text-center">
+                      <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Loại vé</th>
+                      <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Số lượng</th>
+                      <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Tổng</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {selectedReceipt.receiptDetails.map(detail => (
+                        <tr key={detail.id}>
+                            <td className="px-6 py-3 whitespace-normal border-r border-gray-200 text-center">{detail.ticketTypeId}</td>
+                            <td className="px-6 py-3 whitespace-normal border-r border-gray-200 text-center">{detail.amount}</td>
+                            <td className="px-6 py-3 whitespace-normal text-center">{formatCurrency(detail.total)}</td>
+                        </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <div className="text-center">
+                  <button onClick={onClose} className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 mt-4 text-center">Đóng</button>
+                </div>
+              </form>
           </div>
         </div>
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-auto"
-          style={{ marginBottom: 10, backgroundColor: "rgb(128 61 59 / 100%)" }}
-        >
-          Trang đặt vé
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default ReceiptDetail;
--- client/src/views/admin/receipt/Receipts.jsx
+++ client/src/views/admin/receipt/Receipts.jsx
@@ -1,114 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import './User.css';
-import AddUserForm from '../../components/forms/AddUserForm/AddUserForm';
-import EditUserForm from '../../components/forms/EditUserForm/EditUserForm';
-import { FaTrash, FaEdit } from 'react-icons/fa';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import './Receipts.css';
+import axios from "axios";
+import { format } from "date-fns";
+import vi from "date-fns/locale/vi";
+import ReceiptDetail from "./ReceiptDetail";
 
 const Receipts = () => {
-  const [showAddForm, setShowAddForm] = useState(false);
-  const [showEditForm, setShowEditForm] = useState(false);
-  const [selectedUser, setSelectedUser] = useState(null);
-  const [users, setUsers] = useState([]);
+    const [receipts, setReceipts] = useState([]);
+    const [selectedReceipt, setSelectedReceipt] = useState(null);
 
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/api/v1/users'); // Thực hiện gọi API đến endpoint /users
-      const userData = response.data;
-
-      // Kiểm tra xem dữ liệu trả về có phải là một mảng không
-      if (Array.isArray(userData)) {
-        setUsers(userData);
-        console.log(users);
-      } else {
-        console.error('Fetched data is not an array:', userData);
-      }
-    } catch (error) {
-      console.error('Error fetching users:', error);
+    const fetchReceipts = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/v1/receipts');
+            const receiptData = response.data;
+            setReceipts(receiptData);
+        } catch (error) {
+            console.error("Lỗi lấy dữ liệu! ", error);
+        }
     }
-  };
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+    useEffect(() => {
+        fetchReceipts();
+    }, []);
 
-  const handleAddButtonClick = () => {
-    setShowAddForm(true);
-  };
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return format(date, 'dd/MM/yyyy', { locale: vi });
+    };
 
-  const handleCloseAddForm = () => {
-    setShowAddForm(false);
-  };
+    const formatCurrency = (amount) => {
+        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
+    };
 
-  const handleEditButtonClick = (user) => {
-    setSelectedUser(user);
-    setShowEditForm(true);
-  };
+    const handleRowClick = (receipt) => {
+        setSelectedReceipt(receipt);
+    };
 
-  const handleCloseEditForm = () => {
-    setShowEditForm(false);
-    setSelectedUser(null);
-  };
+    const handleCloseDetail = () => {
+        setSelectedReceipt(null);
+    };
 
-  return (
-    <div className='' id='user'>
-      <div>
-        <h1 className='text-center'>QUẢN LÝ NGƯỜI DÙNG</h1>
-      </div>
-      <div className='flex justify-between mt-2 mb-2'>
-        <h2>Danh sách người dùng</h2>
-        <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded' onClick={handleAddButtonClick}>Thêm</button>
-      </div>
-      <div>
-        <table className='w-full bg-white border-collapse border border-gray-200'>
-          <thead className='bg-gray-50'>
-            <tr>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>STT</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Tài khoản</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Họ tên</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Email</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Giới tính</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Số điện thoại</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Địa chỉ</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Vai trò</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200'>Sửa</th>
-              <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Xóa</th>
-            </tr>
-          </thead>
-          <tbody className='bg-white divide-y divide-gray-200'>
-            {users.map((user, index) => (
-              <tr key={user.id}>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>{index + 1}</td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>{user.username}</td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>{user.fullName}</td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>{user.email}</td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>
-                  {user.gender === "male" ? 'Nam' : 'Nữ'}
-                </td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>{user.phoneNumber}</td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>{user.address}</td>
-                <td className='px-6 py-4 whitespace-normal border-r border-gray-200'>
-                  {user.role === "user" ? 'Người dùng' : 'Admin'}
-                </td>
-                <td className='border-r border-200'>
-                  <button className="w-full flex justify-center items-center text-blue-600 hover:text-blue-800" onClick={() => handleEditButtonClick(user)}>
-                    <FaEdit />
-                  </button>
-                </td>
-                <td>
-                  <button className="w-full flex justify-center items-center text-red-600 hover:text-red-800">
-                    <FaTrash />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {showAddForm && <AddUserForm onClose={handleCloseAddForm} />}
-      {showEditForm && <EditUserForm user={selectedUser} onClose={handleCloseEditForm} />}
-    </div>
-  );
+    return (
+        <div id="receipts" >
+            <div className="mb-4">
+                <h1 className="text-center">QUẢN LÝ HÓA ĐƠN</h1>
+            </div>
+            <table className="w-full bg-white border-collapse border border-gray-200">
+                <thead className="bg-gray-50">
+                    <tr>
+                        <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">STT</th>
+                        <th scope="col" className="px-6 py-3 text-xs text-left font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Ngày hóa đơn</th>
+                        <th scope="col" className="px-6 py-3 text-xs text-left font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Khách hàng</th>
+                        <th scope="col" className="px-6 py-3 text-xs text-left font-medium text-gray-500 uppercase tracking-wider">Tổng tiền</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {receipts.map((receipt, index) => (
+                        <tr key={receipt.id} onClick={() => handleRowClick(receipt)} className="cursor-pointer">
+                            <td className="px-6 py-4 whitespace-normal border-r border-gray-200 text-center">{index + 1}</td>
+                            <td className="px-6 py-4 whitespace-normal border-r border-gray-200">{formatDate(receipt.date)}</td>
+                            <td className="px-6 py-4 whitespace-normal border-r border-gray-200">{receipt.user?.full_name || 'Không có tên'}</td>
+                            <td className="px-6 py-4 whitespace-normal">{formatCurrency(receipt.total)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            {selectedReceipt && (
+                <ReceiptDetail selectedReceipt={selectedReceipt} onClose={handleCloseDetail} />
+            )}
+        </div>
+    )
 }
 
 export default Receipts;
